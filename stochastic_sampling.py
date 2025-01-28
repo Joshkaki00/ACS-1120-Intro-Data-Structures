b88f5644a3d4464a682dc868e45d4f1fec32d680
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-01-27 23:00:56 -0800

    stochastic-sampling

---
+++ stochastic_sampling.py
@@ -0,0 +1,17 @@
+import random
+import sys
+from collections import Counter
+
+
+def parse_histogram(file_path):
+    """
+    Parse a text file into a histogram.
+    Each word in the file contributes to the frequency count.
+    """
+    histogram = Counter()
+    with open(file_path, 'r') as file:
+        words = file.read().split()
+        for word in words:
+            histogram[word] += 1
+    return list(histogram.items())
+
